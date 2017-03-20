7c782091a8a8444daaa32926313ec320ba91fef1
Daisuke Shiota <daisuke.shiota@gmail.com>
2017-03-21 07:53:31 +0900

    Comment out the writing into stdout of comparison result

--- crc24q.c
+++ crc24q.c
@@ -119,6 +119,7 @@ int32_t main(int32_t argc, char** argv)
 						ems.msg_bytes[29] |= (crc >> 10) & 0xFF;
 						ems.msg_bytes[30] |= (crc >> 2) & 0xFF;
 						ems.msg_bytes[31] |= (crc & 0x03) << 6;
+						/*
 						fprintf(stdout, "[brdc] %s\n", ems.msg);
 						fprintf(stdout, "[calc] ");
 						for (j = 0; j < 32; j++) {
@@ -128,6 +129,7 @@ int32_t main(int32_t argc, char** argv)
 						fprintf(stdout, "\n");
 						fprintf(stdout, "[comp] %d\n", 
 								strncmp(ems.msg, msg_str, 32));
+								*/
 					}
 				} while(len != 0);
 			}
